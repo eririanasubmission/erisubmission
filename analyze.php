e32247b67ef2de1898e5864e8ad9240baf9420f6
eririanasubmission <53516810+eririanasubmission@users.noreply.github.com>
2019-08-05 18:52:23 +0700

    Update analyze.php

--- analyze.php
+++ analyze.php
@@ -1,5 +1,5 @@
 <?php
-require_once './autoload.php';
+require_once 'vendor/autoload.php';
 require_once "./random_string.php";
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
@@ -8,8 +8,8 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "";
-$containerName = "";
+$connectionString = "K/z7FSjtWAG0MBqxvI8rpV+PvEDVaLNI7XjKXe+TVOdCb+OIwe7CoFTVPGzYAYQvePlOVgLfWePZGgbOvWJQYw==";
+$containerName = "blockblobs";
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 if (isset($_POST['submit'])) {
