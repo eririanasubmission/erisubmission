86a0c009047c9402e87de24464a65c33ff656537
eririana <rianaeri@outlook.com>
2019-08-06 12:49:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@
 				<a class="nav-link" href="https://eririanasubmission.azurewebsites.net/">Home</a>
 			</li>
 			<li class="nav-item">
-				<a class="nav-link" href="https://eririanasubmission.azurewebsites.net/analyze.php">Analisis Foto<span class="sr-only">(current)</span></a>
+				<a class="nav-link" href="https://eririanasubmission.azurewebsites.net/analyze.php">Analisa Foto<span class="sr-only">(current)</span></a>
 			</li>
 		</div>
 		</nav>
