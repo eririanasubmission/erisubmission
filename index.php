17ad29a7b3ec3291e4080b4afd06b0e81a835eae
eririanasubmission <53516810+eririanasubmission@users.noreply.github.com>
2019-08-07 14:19:48 +0700

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ use MicrosoftAzure\Storage\Blob\Models\ListBlobsOptions;
 use MicrosoftAzure\Storage\Blob\Models\CreateContainerOptions;
 use MicrosoftAzure\Storage\Blob\Models\PublicAccessType;
 
-$connectionString = "DefaultEndpointsProtocol=https;AccountName=eririanasubmission;AccountKey=K/z7FSjtWAG0MBqxvI8rpV+PvEDVaLNI7XjKXe+TVOdCb+OIwe7CoFTVPGzYAYQvePlOVgLfWePZGgbOvWJQYw==;EndpointSuffix=core.windows.net";
+$connectionString = "DefaultEndpointsProtocol=https;AccountName=eririanasubmission;AccountKey=K/z7FSjtWAG0MBqxvI8rpV+PvEDVaLNI7XjKXe+TVOdCb+OIwe7CoFTVPGzYAYQvePlOVgLfWePZGgbOvWJQYw==";
 $containerName = "blockblobs";
 // Create blob client.
 $blobClient = BlobRestProxy::createBlobService($connectionString);
